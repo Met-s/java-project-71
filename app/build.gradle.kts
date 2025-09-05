4429471c6d091dbd72f9d874a43dbe55e8c6c416
Met-s <den_va@mail.ru>
2025-09-05 10:50:51 +0300

    Добавил плагин SonarQube для анализа качества кода

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
     id("java")
     id("idea")
     id ("com.github.ben-manes.versions") version "0.52.0"
+    id("org.sonarqube") version "6.3.1.5724"
     application
     jacoco
     checkstyle
@@ -30,15 +31,24 @@ checkstyle {
     toolVersion = "10.12.4"
 }
 
+sonar {
+    properties {
+        property("sonar.projectKey", "Met-s_java-project-71")
+        property("sonar.organization", "met-s")
+    }
+}
+
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher:1.0.0")
     implementation("org.junit.platform:junit-platform-console:1.0.0")
-    implementation ("org.apache.commons:commons-collections4:4.4")
+    implementation("org.apache.commons:commons-collections4:4.4")
     implementation("org.apache.commons:commons-lang3:3.18.0")
     compileOnly("org.projectlombok:lombok:1.18.20")
     annotationProcessor("org.projectlombok:lombok:1.18.20")
+    implementation("info.picocli:picocli:4.7.7")
+    annotationProcessor("info.picocli:picocli-codegen:4.7.7")
 }
 
 tasks.test {
