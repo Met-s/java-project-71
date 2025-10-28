7baae5c2eb002114af8e83acb8e07799eaf46481
Met-s <den_va@mail.ru>
2025-10-28 12:46:03 +0300

    Modified the picocli console application so that when launched with the -h flag, extended help is displayed.

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,15 +1,35 @@
 package hexlet.code;
 
 import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
+
 import java.util.concurrent.Callable;
 
 
-@CommandLine.Command(name = "gendiff",
+@Command(name = "gendiff",
         mixinStandardHelpOptions = true,
         version = "1.0",
         description = "Compares two configuration files and shows a difference.")
 
 public class App implements Callable<String> {
+
+    @Option(names = {"-f", "--format"},
+            paramLabel = "format",
+            defaultValue = "stylish",
+            description = "output format [defaule: stylish]")
+    private String format;
+
+    @Parameters(index = "0",
+            paramLabel = "filepath1",
+            description = "path too first file")
+    String filepath1;
+
+    @Parameters(index = "1",
+            paramLabel = "filepath2",
+            description = "path to second file")
+    String filepath2;
 
     @Override
     public String call() throws Exception {
